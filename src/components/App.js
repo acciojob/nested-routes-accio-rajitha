60d7326726facaab0f3619dada61df7e2813b8fd
accio-rajitha <rajithasabhavath2019@gmail.com>
2025-01-05 16:46:42 +0530

    code1

--- src/components/App.js
+++ src/components/App.js
@@ -13,33 +13,6 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} /> 
-            <Route path="women" element={<Category />}>
-              <Route path=":item" element={<Item />} /> 
-            </Route>
-          </Route>
-        </Routes>
-      </div>
-    </Router>
-  );
-};
-
-export default App;
-/*
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './Layout';
-import Home from './Home';
-import Category from './Category';
-import Item from './Item';
-import './../styles/App.css';
-
-const App = () => {
-  return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} /> 
             <Route path="women" element={<Category />} /> 
             <Route path="women/:item" element={<Item />} /> 
           </Route>
@@ -50,4 +23,3 @@ const App = () => {
 };
 
 export default App;
-*/
